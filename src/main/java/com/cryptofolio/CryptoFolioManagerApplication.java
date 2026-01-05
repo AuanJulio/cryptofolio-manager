0e9417377855afe94ed1292dcb9fb2f56337bcb3
AuanJulio <auanjulio13@gmail.com>
2026-01-05 00:47:17 -0300

    first commit

---
+++ src/main/java/com/cryptofolio/CryptoFolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cryptofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoFolioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoFolioManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cryptofolio/CryptoFolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoFolioManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
